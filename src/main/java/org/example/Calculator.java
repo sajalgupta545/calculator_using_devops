33cdc73db9906dd77b3e598e7f39fe5fed02af9d
sajal@sajal-HP-Pavilion-Laptop-15-cs0xxx <sajalgupta545@gmail.com>
2023-03-13 12:39:18 +0530

    demo commit

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -7,33 +7,33 @@ public class Calculator {
     public static void main(String[] args) {
 
         double num1 , num2 ;
-        Scanner sc = new Scanner(System.in);
+       // Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter two numbers :");
 
-        num1 = sc.nextDouble();
-        num2 = sc.nextDouble();
-
-        System.out.println("Choose any operation you want to perform on numbers");
-        System.out.println("1. Addition (+) ");
-
-        char op = sc.next().charAt(0);
-        double add = 0 ;
-        switch(op)
-        {
-            case '1':
-                add = num1 + num2 ;
-                break;
-
-
-            case '2':
-                System.out.println("Not available right now");
-                break;
-            default :
-                System.out.println("Wrong Option or No option Choosen");
-                System.out.println("............Exiting................");
-        }
-
-        System.out.println("Your Answer is : " + add );
+//        num1 = sc.nextDouble();
+//        num2 = sc.nextDouble();
+//
+//        System.out.println("Choose any operation you want to perform on numbers");
+//        System.out.println("1. Addition (+) ");
+//
+//        char op = sc.next().charAt(0);
+//        double add = 0 ;
+//        switch(op)
+//        {
+//            case '1':
+//                add = num1 + num2 ;
+//                break;
+//
+//
+//            case '2':
+//                System.out.println("Not available right now");
+//                break;
+//            default :
+//                System.out.println("Wrong Option or No option Choosen");
+//                System.out.println("............Exiting................");
+//        }
+//
+//        System.out.println("Your Answer is : " + add );
     }
 }
