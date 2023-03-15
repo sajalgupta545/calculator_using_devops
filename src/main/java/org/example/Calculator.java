c24d2ace9c84fca9d455faa4c450c146095d1a6e
sajal@sajal-HP-Pavilion-Laptop-15-cs0xxx <sajalgupta545@gmail.com>
2023-03-15 23:20:10 +0530

    pipeline file added and log4j created

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,24 +1,41 @@
 package org.example;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Scanner;
 
 public class Calculator {
+
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
+    public Calculator() {
+    }
     public static void main(String[] args) {
 
+        Calculator calculator = new Calculator();
         double num1 , num2 ;
-       // Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter two numbers :");
+        double number1;
 
-//        num1 = sc.nextDouble();
-//        num2 = sc.nextDouble();
+        System.out.print("Enter a number : ");
+        number1 = sc.nextDouble();
+        System.out.println("Factorial of "+number1+" is : " + calculator.factorial(number1));
+        System.out.println("\n");
+
+
+
+//        System.out.println("Enter two numbers :");
 //
-//        System.out.println("Choose any operation you want to perform on numbers");
-//        System.out.println("1. Addition (+) ");
-//
-//        char op = sc.next().charAt(0);
-//        double add = 0 ;
+//       num1 = sc.nextDouble();
+       //num2 = sc.nextDouble();
+
+        //System.out.println("Choose any operation you want to perform on numbers");
+        //System.out.println("1. Addition (+) ");
+
+        //char op = sc.next().charAt(0);
+       // double add = 0 ;
 //        switch(op)
 //        {
 //            case '1':
@@ -35,5 +52,22 @@ public class Calculator {
 //        }
 //
 //        System.out.println("Your Answer is : " + add );
+
+
+    }
+
+
+    public double factorial(double number1) {
+        logger.info("[FACTORIAL] - " + number1);
+        double result = fact(number1);
+        logger.info("[RESULT - FACTORIAL] - " + result);
+        return result;
+    }
+
+    public double fact(double num) {
+        double facto = 1;
+        for(int i = 1; i <= num; ++i)
+        { facto *= i;   }
+        return  facto;
     }
 }
